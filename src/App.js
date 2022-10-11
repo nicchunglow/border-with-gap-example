3ae237cd38fd2bae7fedefe3f4ae6e6742c69131
Nicholas Chung <iamnicholaschung@gmail.com>
2022-10-11 15:51:49 +0800

    Save border with gap css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app">
+      <div class="module">
+        <div class="module__top">
+          <div class="top__content">1 122 3</div>
+        </div>
+        <div class="module__contents"></div>
+      </div>
     </div>
   );
 }
