9776222b310d4719969f6099bb94a24fa72c6871
Nicholas Chung <iamnicholaschung@gmail.com>
2022-10-11 16:29:55 +0800

    Cheap up css code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div class="app">
-      <div class="module">
-        <div class="module__top">
-          <div class="top__content">1 122 3</div>
-        </div>
-        <div class="module__contents"></div>
+      <div class="text-container">
+        <div class="title">1 122 3</div>
+      </div>
+      <div class="text-container">
+        <div class="title">1 122 3</div>
+      </div>
+      <div class="text-container">
+        <div class="title">1 122 3</div>
       </div>
     </div>
   );
